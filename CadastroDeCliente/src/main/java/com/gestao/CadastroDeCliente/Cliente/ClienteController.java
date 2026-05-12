37c176cad0b8c7b4cf59b983ca3803d61c85619e
Igor Bibiano <igorbibianodasilva@gmail.com>
2026-05-12 11:59:37 -0300

    Feat: Criei um endPoint de consula por ID no meu ClienteController

--- CadastroDeCliente/src/main/java/com/gestao/CadastroDeCliente/Cliente/ClienteController.java
+++ CadastroDeCliente/src/main/java/com/gestao/CadastroDeCliente/Cliente/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +34,9 @@ public class ClienteController {
     }
 
     @GetMapping("/listar/{id}")
-    public ResponseEntity<ClienteDTO> listarClienteID(){
-
+    public ResponseEntity<ClienteDTO> listarClienteID(@PathVariable Long id){
+        ClienteDTO clienteID = clienteService.listarClienteID(id);
+        return ResponseEntity.ok(clienteID);
     }
 
 }
